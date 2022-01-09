041a62a3a4508ec83eb7a74ed87017d3e642e286
NazarenkoAnna2021.com <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-01-09 02:11:56 +0200

    fix filters request and movie validation

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,10 +1,12 @@
 const validators = require('./validation');
-const { getMovieById, getMovieByFilters } = require('../database/repositories/movie.repository')
+const { getMovieById, getMovieByFilters } = require('../database/repositories/movie.repository');
+const { verifyToken } = require('./jwt/jwt');
+const { getStatusAuth } = require('./status/auth.status');
 
 exports.getSingleMovie = async (query, token) => {
     const { value, error } = verifyToken(token);
     const err = getStatusAuth(value, error);
-    if (error) return {error: err};
+    if (error) return { error: err };
     if (value) {
         const { value, error } = validators.validate(query, validators.idValidator);
         if (error) return { error };
@@ -17,12 +19,12 @@ exports.getSingleMovie = async (query, token) => {
 exports.getMovies = async (query, token) => {
     const { value, error } = verifyToken(token);
     const err = getStatusAuth(value, error);
-    if (error) return {error: err};
+    if (error) return { error: err };
     if (value) {
-        const {value, error} = validators.validate(query, validators.movieQuweryValidator);
-        if(error) return {error};
-        const {error: dbError, result} = await getMovieByFilters(value);
-        if(dbError) return {error: {status: 500, data: {error}}};
-        return {result: {data: result, status: 200}};
+        const { value, error } = validators.validate(query, validators.movieQuweryValidator);
+        if (error) return { error };
+        const { error: dbError, result } = await getMovieByFilters(value);
+        if (dbError) return { error: { status: 500, data: { error } } };
+        return { result: { data: result, status: 200 } };
     }
 };
--- database/repositories/movie.repository.js
+++ database/repositories/movie.repository.js
@@ -8,24 +8,22 @@ exports.getMovieById = async (id) => {
     return { error: e.message };
   }
 };
-exports.getMovieByFilters = async ({ adult, title, release_date_first, release_date_last, revenue_min, revenue_max, status, budget_min, budget_max, page, pre_page }) => {
+exports.getMovieByFilters = async ({ adult, title, release_date_first, release_date_last, revenue_min, revenue_max, status, budget_min, budget_max, popularity_min, popularity_max, page, pre_page }) => {
   try {
     const first = (page * pre_page) - pre_page;
-    const movie = await pgClient.query(
-    `SELECT * FROM films page 
-                   WHERE page.title ILIKE '%${title}%' ${
-                          release_date_first && release_date_last ?
-                              `AND page.release_date BETWEEN '${release_date_first}' AND '${release_date_last}'` : '' }${
-                          revenue_min && revenue_max ? 
-                             `AND page.revenue BETWEEN ${revenue_min} AND ${revenue_max}` : ''} ${
-                          status ? 
-                              `AND page.status = '%${status}%'` : ''}${
-                          adult ? `AND page.adult = '${adult}'` : '' 
-                              } ${
-                          budget_min && budget_max ?
-                              `AND page.budget BETWEEN ${budget_min} AND ${budget_max}` : ''
-                              } offset ${first || 0} LIMIT ${pre_page || 100}`);
-    
+
+    const movie = await pgClient.query(`
+      SELECT * FROM films films 
+      WHERE films.title ILIKE '%${title}%' 
+            AND films.release_date BETWEEN '${release_date_first}' AND '${release_date_last}'
+            AND films.revenue BETWEEN ${revenue_min} AND ${revenue_max}
+            AND films.status LIKE '%${status}%'
+            AND films.adult = '${adult}'
+            AND films.budget BETWEEN ${budget_min} AND ${budget_max} 
+            AND films.popularity BETWEEN ${popularity_min} AND ${popularity_max}
+      offset ${first} LIMIT ${pre_page}
+      `);
+
     return { result: { totalCount: movie.rows.length, data: movie.rows, currentPage: page } };
   } catch (e) {
     return { error: e.message };
