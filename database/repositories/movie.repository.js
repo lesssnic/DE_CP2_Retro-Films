f872e7cfe78cc73f25c74ac1d7557de9db414e14
NazarenkoAnna2021.com <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-01-11 21:23:23 +0200

    remake movie by id request to independent

--- database/repositories/movie.repository.js
+++ database/repositories/movie.repository.js
@@ -3,25 +3,31 @@ const pgClient = require('../dbconnect');
 exports.getMovieById = async (id) => {
   try {
     const movie = await pgClient.query(`
-    SELECT f.*, l.english_name as language_in_en,g.genres , JSON_AGG(
-      JSON_BUILD_OBJECT(
-          'login', u.login ,
-          'content', r.content_review
-          )) as reviews 
-       FROM films f, languages l, review r, users u, 
-       (
-        SELECT STRING_AGG(g."name" , ', ') as genres FROM genres g, films f, films_genres fg 
-        WHERE f.id = 106
-              AND f.id = fg.films_id 
-              AND g.id = fg.genre_id 
-          LIMIT 1
-       ) g
-       WHERE f.id = 106
-              AND l.iso_639_1 = f.original_language
-              AND u.id =r.user_id 
-              AND f.id = r.movie_id 
-        GROUP BY f.id, l.english_name , g.genres
-        LIMIT 1;
+    SELECT f.*, l.english_name as language_in_en,g.genres , JSON_AGG(r.reviews) as reviews 
+     FROM films f, languages l, 
+     (
+     	SELECT STRING_AGG(g."name" , ', ') as genres FROM genres g, films f, films_genres fg 
+		WHERE f.id = ${id}
+			and f.id = fg.films_id 
+			AND g.id = fg.genre_id 
+		LIMIT 1
+     ) g,
+     (
+     select
+         JSON_BUILD_OBJECT(
+          'review_id', r.id,
+            'login', u.login ,
+            'content', r.content_review
+            ) as reviews
+          from  review r, users u, films f
+          where r.movie_id = ${id}
+                AND u.id = r.user_id
+                group by r.id, u.login
+     ) r
+     WHERE f.id = ${id}
+            AND l.iso_639_1 = f.original_language
+      GROUP BY f.id, l.english_name , g.genres
+      LIMIT 1;
     `);
     return { result: movie.rows };
   } catch (error) {
